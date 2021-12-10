945035c6dc0074ea8b7396f0c9d4ce152811050a
niiyebuah <77839589+niiyebuah@users.noreply.github.com>
2021-12-10 19:17:47 +0000

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,34 @@
+<?php
+include_once 'header.php';
+?>    
+
+<div class="container-fluid padding">
+    <div class="row welcome text-center">
+        <div class="col-12">
+            <h2 class="display-6">ORDER CHECKOUT</h2>
+        </div>
+    </div>
+</div>
+
+<hr class="my-4"> 
+
+
+
+
+<div class="container-fluid padding">
+    <div class="row welcome text-center">
+        <div class="col-12">
+        <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Hey there valued Customer</h4>
+                        <p class="card-text">Your order is currently being processed.</p>
+                        <p class="card-text">We will get back to you soon.Thank you and have a wonderful day</p>
+                    </div>
+                </div>        
+            </div>
+    </div>
+</div>
+
+      <?php
+  include_once 'footer.php';
+  ?>
